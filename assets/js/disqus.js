42ad9e50de17fc50af57c3aa15f7d339459b19e8
Victor Alejo Arias <victoralejoarias@MBP-HSPLC-12.local>
2021-09-28 22:44:45 +0200

    Fix disqus set up
    * Comments after the post
    * nickname fix

--- assets/js/disqus.js
+++ assets/js/disqus.js
@@ -3,13 +3,13 @@
     *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
 
     var disqus_config = function () {
-    this.page.url = "[https://babaganush.site]{{ page.url }}";  // Replace PAGE_URL with your page's canonical URL variable
-    this.page.identifier = "{{page_url}}";//PAGE_IDENTIFIER; Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    this.page.url = "https://babaganush.site{{ page.url }}";  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = "{{page.url}}";//PAGE_IDENTIFIER; Replace PAGE_IDENTIFIER with your page's unique identifier variable
     };
 
     (function() { // DON'T EDIT BELOW THIS LINE
     var d = document, s = d.createElement('script');
-    s.src = 'https://babaganush.disqus.com/embed.js';
+    s.src = '//babaganush.disqus.com/embed.js';
     s.setAttribute('data-timestamp', +new Date());
     (d.head || d.body).appendChild(s);
     })();
